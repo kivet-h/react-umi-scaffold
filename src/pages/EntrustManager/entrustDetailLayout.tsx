cf33a786fc095df91aaa8007527f0a2f0b52789f
kivet <wanghuixiang@druid.tech>
2022-03-08 11:32:36 +0800

    fix: 优化，详情页菜单切换，地址栏保留部分参数

--- src/pages/EntrustManager/entrustDetailLayout.tsx
+++ src/pages/EntrustManager/entrustDetailLayout.tsx
@@ -2,18 +2,31 @@
  * @Description: 委托详情页 Layout
  * @Author: kivet
  * @Date: 2022-01-25 15:55:00
- * @LastEditTime: 2022-03-08 10:46:06
+ * @LastEditTime: 2022-03-08 11:30:39
  */
 
+import { Link } from 'umi';
 import type { BasicLayoutProps } from '@ant-design/pro-layout';
 import DefaultLayout from '@/layouts/DefaultLayout';
+import { urlHelper } from '@/utils/urlHelper';
+
+const queryString = require('query-string');
 
 interface IProps extends BasicLayoutProps {}
 
 const entrustDetailLayout: React.FC<IProps> = (props) => {
   console.log('entrust-detail', props.route);
 
-  return <DefaultLayout {...props} />;
+  return (
+    <DefaultLayout
+      {...props}
+      menuItemRender={(item: any, dom: any) => {
+        // ? 详情页菜单切换，需保留地址栏部分参数，如id
+        const urlParams = queryString.stringify(urlHelper.getUrlParams('id'));
+        return <Link to={urlParams ? `${item.path}?${urlParams}` : item.path}>{dom}</Link>;
+      }}
+    />
+  );
 };
 
 export default entrustDetailLayout;
--- src/pages/PostManager/postDetailLayout.tsx
+++ src/pages/PostManager/postDetailLayout.tsx
@@ -2,18 +2,31 @@
  * @Description: 帖子详情页 Layout
  * @Author: kivet
  * @Date: 2022-01-25 15:55:00
- * @LastEditTime: 2022-03-08 10:45:09
+ * @LastEditTime: 2022-03-08 11:31:38
  */
 
+import { Link } from 'umi';
 import type { BasicLayoutProps } from '@ant-design/pro-layout';
 import DefaultLayout from '@/layouts/DefaultLayout';
+import { urlHelper } from '@/utils/urlHelper';
+
+const queryString = require('query-string');
 
 interface IProps extends BasicLayoutProps {}
 
 const postDetailLayout: React.FC<IProps> = (props) => {
   console.log('postttt', props.route);
 
-  return <DefaultLayout {...props} />;
+  return (
+    <DefaultLayout
+      {...props}
+      menuItemRender={(item: any, dom: any) => {
+        // ? 详情页菜单切换，需保留地址栏部分参数，如id
+        const urlParams = queryString.stringify(urlHelper.getUrlParams('id'));
+        return <Link to={urlParams ? `${item.path}?${urlParams}` : item.path}>{dom}</Link>;
+      }}
+    />
+  );
 };
 
 export default postDetailLayout;
